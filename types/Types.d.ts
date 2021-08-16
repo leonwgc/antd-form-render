9684024d1ac6b45b29e8c2d03fd37bddbe539962
leonwgc <giantfish@126.com>
2021-08-16 12:46:20 +0800

    feat: update types comment

--- types/Types.d.ts
+++ types/Types.d.ts
@@ -9,14 +9,16 @@ export declare type SpaceLayoutProps = SpaceProps & {
   layoutData: Item[];
 };
 export declare type Item = {
-  type?: React.ComponentType | string;
-  name?: string;
-  label?: string;
-  render?: () => React.ReactElement;
-  getJSON?: () => Item | null;
-  elProps?: Record<string | number | symbol, unknown>;
-  itemProps?: Record<string | number | symbol, unknown>;
-  rules?: Rule[];
-  [p: string]: unknown;
+  type?: React.ComponentType | string /** 组件类型，比如Input,Button,"input"  */;
+  name?: string /** 传给Form.Item的name,作为form data的key */;
+  label?: string /** label名称 */;
+  render?: () => React.ReactNode /** 自定义render ,返回任何内容都可 */;
+  getJSON?: () => Item | null /** 动态返回Item */;
+  elProps?: Record<
+    string | number | symbol,
+    unknown
+  > /** 组件的props， 比如Button,Input 的props , 参考antd文档配置 */;
+  itemProps?: Record<string | number | symbol, unknown> /** Form.Item的props, 参考antd文档配置  */;
+  rules?: Rule[] /** Form.Item的rules,在itemProps里面定义也可,放这里主要为了兼容旧代码   */;
 };
 export declare type LayoutType = 'row' | 'space';
