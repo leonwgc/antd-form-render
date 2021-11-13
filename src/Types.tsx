980596b01557ad9b873a01844b26f822d13015bb
leonwgc <giantfish@126.com>
2021-11-13 14:02:28 +0800

    docs: types

--- src/Types.tsx
+++ src/Types.tsx
@@ -2,36 +2,49 @@ import React from 'react';
 import { Rule } from 'rc-field-form/lib/interface';
 import type { SpaceProps } from 'antd';
 
-export type FormRenderProps = {
-  /**
-   * 一维数组:从上往下一行放一个表单项 ,如果设置了cols=2/3/4 ,则一行放置cols(2/3/4)个表单项
-   * 二维数组:子数组配置的表单项目会被渲染为一行
-   */
-  layoutData: Item[] | Item[][];
-  cols?: 1 | 2 | 3 | 4;
-};
-
-export type SpaceLayoutProps = SpaceProps & {
-  layoutData: Item[];
-};
-
 export type Item = {
   /** 组件类型，比如Input,Button,"input"  */
   type?: React.ComponentType | string;
-  /** 传给Form.Item的name,作为form data的key */
-  name?: string;
-  /** label名称 */
-  label?: string;
-  /** 自定义render */
+
+  /** Form.Item name, 字段名，支持数组 */
+  name?: string | Array<string | number>;
+
+  /** Form.Item label, 标签的文本*/
+  label?: React.ReactNode;
+
+  /** 自定义渲染 */
   render?: () => React.ReactNode;
+
   /** 动态返回Item，优先级高于render */
   getJSON?: () => Item | null;
-  /** 组件的props,比如Button,Input的props,会透传给type定义的组件 */
+
+  /** 组件props,会透传给type定义的组件 */
   elProps?: Record<string, unknown>;
+
   /** Form.Item的props,会透传给Form.Item */
   itemProps?: Record<string, unknown>;
-  /** Form.Item的rules,在itemProps里面定义也可,放这里主要为了兼容  */
+
+  /** Form.Itemrules,也可在itemProps里定义  */
   rules?: Rule[];
+};
+
+export type FormRenderProps = {
+  /**
+   * 1或2维数组，存储组件配置信息/自定义渲染组件
+   */
+  layoutData: Item[] | Item[][];
+
+  /**
+   * 定义一行渲染几个组件，layoutData为一维数组时生效, 可以是: 1 | 2 | 3 | 4, 默认1,
+   */
+  cols?: number;
+};
+
+export type SpaceLayoutProps = SpaceProps & {
+  /**
+   * 1维数组，存储组件配置信息/自定义渲染组件
+   */
+  layoutData: Item[];
 };
 
 export type LayoutType = 'row' | 'space';
--- types/index.d.ts
+++ types/index.d.ts
@@ -2,13 +2,18 @@ import React from 'react';
 import { FormRenderProps } from './Types';
 export { default as FormSpaceRender } from './SpaceLayout';
 /**
- * 一维数组:从上往下一行放一个表单项 ,如果设置了cols=2/3/4 ,则一行放置cols(2/3/4)个表单项
- * 二维数组:子数组配置的表单项目会被渲染为一行
+ * 等分空间布局, 每个组件等分一行空间
+ *
+ * 一维数组:从上往下一行放一个组件 ,设置了cols则一行显示cols(1/2/3/4)个组件
+ *
+ * 二维数组:子数组配置的所有组件渲染为一行（不定列布局）
+ *
+ * 数组（或子数组）内组件会等分一行所占空间，内部采用Row,Col布局
  *
  * @export
  * @param {FormRenderProps} {
  *   layoutData,
- *   cols = 1,
+ *   cols = 1 | 2 | 3 | 4,
  * }
  * @return {*}  {React.ReactElement}
  */
