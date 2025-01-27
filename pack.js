73a1e47de0f045b5bd4c1c67ebd9afb1985e6eb0
leonwgc <giantfish@126.com>
2025-01-27 08:32:22 +0800

    docs: rebuild with packrs

--- pack.js
+++ pack.js
@@ -1,24 +1,16 @@
-/* eslint-disable  */
-const { default: pack } = require('packw');
+const { run, build } = require('packrs');
 const argv = require('yargs').argv;
-const path = require('path');
 
-const isDev = !!argv.dev;
+const doc = argv.doc;
+const isDev = !doc;
 
-pack(isDev, {
-  entry: {
-    index: `./demo/index`,
-  },
-  output: {
-    path: path.resolve(__dirname, isDev ? '__dev__' : 'docs'),
-    publicPath: '',
-  },
-  devServer: {
-    port: 9101,
-  },
-  resolve: {
-    alias: {
-      'antd-form-render': path.resolve(__dirname, './src'),
+(isDev ? run : build)({
+  index: `./demo/index`,
+  dist: isDev ? './.dev' : './docs',
+  port: 9001,
+  rsConfig: {
+    html: {
+      title: 'antd-form-render',
     },
   },
 });
